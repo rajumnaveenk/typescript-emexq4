dc717c9959197ed8cf8c4a3b71420f4612cce033
rajumnaveenk <naveen.kumar97044@gmail.com>
2022-11-07 13:16:59 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -2,5 +2,12 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const number1 = 2;
+const number2 = 3;
+
+const result = add(number1, number2);
+document.write(result);
